7b8b5b64b3b532fcdbb6c4f3b8f94eb3939b159b
christophe <christophe.bastin@umons.ac.be>
2013-12-04 13:36:34 +0100

    add column information to admin interface

--- admin.py
+++ admin.py
@@ -3,10 +3,17 @@ __author__ = 'Christophe Bastin'
 from django.contrib import admin
 from models import *
 
+class ComputerAdmin(admin.ModelAdmin):
+    list_display = ('ref_user', 'name')
+
+
+class DesktopAdmin(admin.ModelAdmin):
+    list_display = ('ref_user', 'name', 'ref_processor', 'ref_hard_drive')
 
 admin.site.register(Processor)
 admin.site.register(GraphicCard)
 admin.site.register(HardDrive)
-admin.site.register(Desktop)
+admin.site.register(Computer, ComputerAdmin)
+admin.site.register(Desktop, DesktopAdmin)
 admin.site.register(Laptop)
 admin.site.register(Server)
