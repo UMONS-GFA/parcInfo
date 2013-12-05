cea7530c0e2d0ff2a00085420e729f7a487c83c3
christophe <christophe.bastin@umons.ac.be>
2013-12-05 11:41:25 +0100

    add tests for admin pages

--- tests.py
+++ tests.py
@@ -1,16 +1,34 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+__author__ = 'Christophe Bastin'
 
 from django.test import TestCase
+from django.test import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ParcInfoTestCase(TestCase):
+    fixtures = ['fixtures']
+
+    def setUp(self):
+        self.client = Client()
+
+    def testAdmin(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.post('/admin', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin/index.html')
+
+    def testAdminAnonymous(self):
+        response = self.client.post('/admin', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin/login.html')
+
+    def testAdminNotAuthorized(self):
+        self.client.login(username='test', password='test')
+        response = self.client.post('/admin', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin/login.html')
+
+    def testAdminParcInfo(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.post('/admin/parcInfo', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin/app_index.html')
