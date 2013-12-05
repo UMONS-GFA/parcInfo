e27a2c205319b98ceec5b8f33a94c0b12ee865fa
christophe <christophe.bastin@umons.ac.be>
2013-12-05 14:24:04 +0100

    add localisation field in admin forms

--- admin.py
+++ admin.py
@@ -10,22 +10,26 @@ class ComputerAdmin(admin.ModelAdmin):
 
 class DesktopAdmin(admin.ModelAdmin):
     list_display = ('ref_user', 'name', 'ref_processor', 'memory_size', 'ref_hard_drive', 'serial')
-    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'ref_hard_drive', 'usage_note')
+    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'ref_hard_drive', 'localisation',
+              'usage_note')
 
 
 class LaptopAdmin(admin.ModelAdmin):
     list_display = ('ref_user', 'name', 'ref_processor', 'memory_size', 'ref_hard_drive', 'serial')
-    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'ref_hard_drive', 'usage_note')
+    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'ref_hard_drive', 'localisation',
+              'usage_note')
 
 
 class ServerAdmin(admin.ModelAdmin):
     list_display = ('name', 'serial')
-    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'nb_disk', 'ref_hard_drive', 'usage_note')
+    fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'nb_disk', 'ref_hard_drive',
+              'localisation', 'usage_note')
 
 admin.site.register(Processor)
 admin.site.register(GraphicCard)
 admin.site.register(HardDrive)
-admin.site.register(Computer, ComputerAdmin)
+admin.site.register(Localisation)
+#admin.site.register(Computer, ComputerAdmin)
 admin.site.register(Desktop, DesktopAdmin)
 admin.site.register(Laptop, LaptopAdmin)
 admin.site.register(Server, ServerAdmin)
