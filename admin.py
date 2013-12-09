a80814df96c468609e8c077b079a6b66ca5ae60d
christophe <christophe.bastin@umons.ac.be>
2013-12-09 09:21:26 +0100

    remove unused code

--- admin.py
+++ admin.py
@@ -46,8 +46,6 @@ class LaptopAdmin(admin.ModelAdmin):
 class ServerAdmin(admin.ModelAdmin):
     list_display = ('name', 'serial')
     inlines = (CnxServerProcessorInline, CnxServerGraphicCardInline,CnxServerHardDriveInline,)
-    #fields = ('serial', 'name', 'ref_user', 'ref_processor', 'memory_size', 'nb_disk', 'ref_hard_drive',
-    #          'localisation', 'usage_note')
 
 admin.site.register(Processor, ProcessorAdmin)
 admin.site.register(GraphicCard)
